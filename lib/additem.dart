399e539c5e5f4bc54f5073844a1664793eac237d
shiridikumar <p.shiridi@research.iiit.ac.in>
2022-05-28 22:24:05 +0530

    updated additem option

--- lib/additem.dart
+++ lib/additem.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:first/home.dart';
@@ -15,110 +13,118 @@ class additem extends StatefulWidget {
 }
 
 class _additemState extends State<additem> {
+  List<Widget> specs = [];
+  List<Widget> specsupd = [];
+  List<String> value_indices = ['', '', '', ''];
+  void addfields() {
+    List<Widget> myArray = [
+      gen_textfield(3, 'Enter Field name', 'Field name', 250, 0),
+      gen_textfield(3, 'Enter Field value', 'Field value', 250, 0)
+    ];
+    this.specs.add(Container(
+        constraints: BoxConstraints(maxHeight: 200.0),
+        child: GridView.count(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            padding: EdgeInsets.all(0.0),
+            mainAxisSpacing: 1.0,
+            childAspectRatio: 2 / 1,
+            crossAxisSpacing: 1.0,
+            crossAxisCount: 2, // here you keep track of count
+            children: myArray)));
+    setState(() {
+      this.specsupd = this.specs;
+    });
+  }
+
+  Widget gen_textfield(
+      int ind, String hint, String label, double width, int height) {
+    return Container(
+      padding: EdgeInsets.all(5.0),
+      constraints: BoxConstraints(maxWidth: width),
+      child: TextFormField(
+        obscureText: true,
+        decoration: InputDecoration(
+            fillColor: Colors.white,
+            filled: true,
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5.0),
+                borderSide: BorderSide(color: Colors.blue)),
+            labelText: label,
+            hintText: hint,
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5.0),
+                borderSide: BorderSide(color: Colors.grey))),
+        onChanged: (text) {},
+        style: TextStyle(
+            color: Colors.black,
+            decorationStyle: TextDecorationStyle.dotted,
+            decorationColor: Colors.white),
+      ),
+    );
+  }
+
   Widget uploaddetails(BuildContext context) {
     return Container(
         decoration: BoxDecoration(color: Colors.white),
         constraints: BoxConstraints(minWidth: 400.0),
         padding: EdgeInsets.all(10.0),
         child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text("Fill in the following details",
-                  style: TextStyle(fontFamily: "mainfont", fontSize: 18.0)),
-              SizedBox(height: 20.0),
-              Container(
-                padding: EdgeInsets.all(5.0),
-                constraints: BoxConstraints(maxWidth: 300.0),
-                child: TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                          borderSide: BorderSide(color: Colors.blue)),
-                      labelText: 'Product name',
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                          borderSide: BorderSide(color: Colors.grey))),
-                  onChanged: (text) {},
-                  style: TextStyle(
-                      color: Colors.black,
-                      decorationStyle: TextDecorationStyle.dotted,
-                      decorationColor: Colors.white),
-                ),
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("Fill in the following details",
+                      style: TextStyle(fontFamily: "mainfont", fontSize: 18.0)),
+                  SizedBox(height: 20.0),
+                  gen_textfield(0, '', "Prodcut Name", 300, 0),
+                  Container(
+                      padding: EdgeInsets.all(5.0),
+                      constraints: BoxConstraints(maxWidth: 300.0),
+                      child: DropdownButton<String>(
+                        hint: Text('Product Type'),
+                        items: <String>[
+                          'Watch',
+                          'Camera',
+                          'Telivision',
+                          'Washing machine',
+                          'PC',
+                          'Fan',
+                          'AC',
+                          'Headset',
+                          'TubeLight',
+                          'Other'
+                        ].map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (_) {},
+                      )),
+                  gen_textfield(1, '', "MRP", 300, 0),
+                  gen_textfield(0, '', "Final Price", 300, 0),
+                ]),
+            Column(children: this.specsupd),
+            SizedBox(height: 30.0),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: ElevatedButton(
+                    onPressed: () {
+                      addfields();
+                    },
+                    child: Text('Add fields'),
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.red, minimumSize: Size(100.0, 50.0))),
               ),
-              Container(
-                padding: EdgeInsets.all(5.0),
-                constraints: BoxConstraints(maxWidth: 300.0),
-                child: TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                          borderSide: BorderSide(color: Colors.blue)),
-                      labelText: 'Product type',
-                      hintText: 'Enter Product type , eg: Earphones...',
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                          borderSide: BorderSide(color: Colors.grey))),
-                  onChanged: (text) {},
-                  style: TextStyle(
-                      color: Colors.black,
-                      decorationStyle: TextDecorationStyle.dotted,
-                      decorationColor: Colors.white),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(5.0),
-                constraints: BoxConstraints(maxWidth: 300.0),
-                child: TextFormField(
-                  keyboardType: TextInputType.number,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                          borderSide: BorderSide(color: Colors.blue)),
-                      labelText: 'MRP',
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                          borderSide: BorderSide(color: Colors.grey))),
-                  onChanged: (text) {},
-                  style: TextStyle(
-                      color: Colors.black,
-                      decorationStyle: TextDecorationStyle.dotted,
-                      decorationColor: Colors.white),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(5.0),
-                constraints: BoxConstraints(maxWidth: 300.0),
-                child: TextFormField(
-                  keyboardType: TextInputType.number,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                          borderSide: BorderSide(color: Colors.blue)),
-                      labelText: 'Final price',
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                          borderSide: BorderSide(color: Colors.grey))),
-                  onChanged: (text) {},
-                  style: TextStyle(
-                      color: Colors.black,
-                      decorationStyle: TextDecorationStyle.dotted,
-                      decorationColor: Colors.white),
-                ),
-              ),
-            ]));
+            ),
+          ],
+        ));
   }
 
   Widget uploadpic(BuildContext context) {
@@ -127,14 +133,18 @@ class _additemState extends State<additem> {
         constraints: BoxConstraints(minWidth: 400.0),
         decoration: BoxDecoration(color: Colors.white),
         child: Column(children: <Widget>[
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.camera_alt),
-            iconSize: 200.0,
-            padding: EdgeInsets.zero,
+          Image.asset(
+            'assets/no-image.jpg',
+            height: 250.0,
           ),
-          Text("Upload image",
-              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 25.0))
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.red, minimumSize: Size(200.0, 50.0)),
+                child: Text('Upload image', style: TextStyle(fontSize: 20.0))),
+          )
         ]));
   }
 
